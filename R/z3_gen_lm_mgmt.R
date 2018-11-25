39c8e0deb36422fd02f405b9b0c3e35fed28254b
Ben Staton <statonbe@gmail.com>
2018-11-25 14:36:31 -0600

    remove negative U_msy

--- R/z3_gen_lm_mgmt.R
+++ R/z3_gen_lm_mgmt.R
@@ -14,6 +14,7 @@ gen_lm_mgmt = function(alpha, beta) {
 
   U_msy = log_alpha * (0.5 - (0.65 * log_alpha ^1.27)/(8.7 + log_alpha^1.27))
   U_msy[U_msy == "NaN"] = 0
+  U_msy[U_msy < 0] = 0
   S_msy = U_msy/beta
 
   return(list(U_msy = U_msy, S_msy = S_msy))
