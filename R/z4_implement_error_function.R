22573a4756cc0947f6cfcd38981591bf94c5fc05
Ben Staton <bstaton@critfc.org>
2019-08-11 10:36:12 -0700

    rename and improve docs for implement error

--- R/z4_implement_error_function.R
+++ R/z4_implement_error_function.R
@@ -3,8 +3,8 @@
 #' Generate a beta random deviate around a target exploitation rate
 #' with variability given by \code{SUM}, the sum of the two beta shape parameters.
 #'
-#' @param U_target Numeric vector: the mean target exploitation rate
-#' @param SUM Numeric vector: the sum of the two shape parameters of the beta distribution.
+#' @param U_target Numeric vector of length 1: the mean target exploitation rate
+#' @param SUM Numeric vector of length 1: the sum of the two shape parameters of the beta distribution.
 #'   The larger this value is, the less implementation error there is.
 
 implement_error = function(U_target, SUM) {
@@ -13,10 +13,3 @@ implement_error = function(U_target, SUM) {
 
   rbeta(1, Ua, Ub)
 }
-
-
-# U_target = runif(1000, 0.01, 0.99)
-# U_real = sapply(U_target, function(x) implement_error(U_target = x, SUM = 10))
-#
-# windows()
-# plot(U_target ~ U_real)
